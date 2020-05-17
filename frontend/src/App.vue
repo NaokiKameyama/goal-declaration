b0269d49ed84b7a2b8868c6c48cc241319bf7e2b
teikokukinngyo7 <teikokukinngyo7@gmail.com>
2020-05-17 19:28:37 +0900

    first commit umeda GO

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Goal Delaration!"/>
+    <HelloWorld msg="Welcome to Goal Umeda Go!"/>
   </div>
 </template>
 
