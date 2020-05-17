8b098da9b59b2cfe77165a91fd5866eb0827cdcc
teikokukinngyo7 <teikokukinngyo7@gmail.com>
2020-05-17 19:55:35 +0900

    2nd test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Goal Umeda Go!"/>
+    <HelloWorld msg="Welcome to Goal 2nd test!"/>
   </div>
 </template>
 
