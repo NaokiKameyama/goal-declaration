8e5e86910c608aef46658080688b11688aee92a8
teikokukinngyo7 <teikokukinngyo7@gmail.com>
2020-05-17 20:17:25 +0900

    4th test

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Goal ３nd test!"/>
+    <HelloWorld msg="Welcome to Goal 4th test!"/>
   </div>
 </template>
 
