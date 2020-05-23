2212f9218c925325ea4f45d4a487f5cb9dd163fe
Naoki-Kameyama <atukan0930@gmail.com>
2020-05-23 15:27:01 +0900

    todo→GoalTweetに変更

--- frontend/src/components/App/script.js
+++ frontend/src/components/App/script.js
@@ -1,11 +1,11 @@
 import Top from "@/components/Top/Index.vue";
-import Add from "@/components/Todo/Index.vue";
+import GoalTweet from "@/components/GoalTweet/Index.vue";
 // import Index from "@/components/Todo/Index.vue";
 
 export default {
   name: "app",
   components: {
     Top,
-    Add
+    GoalTweet
   }
 };
