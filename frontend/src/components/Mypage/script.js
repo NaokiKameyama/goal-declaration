a338665c4f57e5af2d9a0672a0ebc21804a40876
Naoki-Kameyama <atukan0930@gmail.com>
2020-06-07 01:54:29 +0900

    マイページにタイムラインの追加

--- frontend/src/components/Mypage/script.js
+++ frontend/src/components/Mypage/script.js
@@ -1,8 +1,19 @@
+import moment from "moment"
+
 export default {
   name: 'HelloWorld',
   data: function() {
     return {
+      reverse: true,
       displayName: ''
+    }
+  },
+  computed: {
+    emailHead() {
+      return this.$store.getters.getEmailHead
+    },
+    myTodos(){
+      return this.$store.getters.getMyTodos
     }
   },
   created() {
@@ -12,6 +23,16 @@ export default {
     signOut: function () {
       this.$store.dispatch('signOut');
       this.$router.push('/')
+    }
+  },
+  filters: {
+    unixTime2Date: function(date) {
+      try {
+        var convertDate  = date.toDate()
+        return moment(convertDate).format('YYYY/MM/DD HH:mm:ss')
+      } catch( e ) {
+        return 0
+      }
     }
   }
 }
